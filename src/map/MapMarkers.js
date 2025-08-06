e5ada9ee7f666d45c0b2a6e73a7ed032a80e0a73
AmanRidesIQ <aman@ridesiq.com>
2025-08-06 16:17:45 +0530

    added  Changes to MapMarkers.js.
    logic is separated and used memoization for optimizing code. add zoom based toggle

--- src/map/MapMarkers.js
+++ src/map/MapMarkers.js
@@ -1,4 +1,4 @@
-import { useId, useEffect } from 'react';
+import { useId, useEffect, useMemo } from 'react';
 import { useTheme } from '@mui/styles';
 import { useMediaQuery } from '@mui/material';
 import { map } from './core/MapView';
@@ -12,76 +12,86 @@ const MapMarkers = ({ markers, showTitles }) => {
   const desktop = useMediaQuery(theme.breakpoints.up('md'));
   const iconScale = useAttributePreference('iconScale', desktop ? 0.75 : 1);
 
-  useEffect(() => {
-    map.addSource(id, {
-      type: 'geojson',
-      data: {
-        type: 'FeatureCollection',
-        features: [],
+  const features = useMemo(() => {
+    return markers.map(({ latitude, longitude, image, title }) => ({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [longitude, latitude],
       },
-    });
+      properties: {
+        image: image || 'default-neutral',
+        title: title || '',
+      },
+    }));
+  }, [markers]);
 
-    if (showTitles) {
-      map.addLayer({
-        id,
-        type: 'symbol',
-        source: id,
-        filter: ['!has', 'point_count'],
-        layout: {
-          'icon-image': '{image}',
-          'icon-size': iconScale,
-          'icon-allow-overlap': true,
-          'text-field': '{title}',
-          'text-allow-overlap': true,
-          'text-anchor': 'bottom',
-          'text-offset': [0, -2 * iconScale],
-          'text-font': findFonts(map),
-          'text-size': 12,
-        },
-        paint: {
-          'text-halo-color': 'white',
-          'text-halo-width': 1,
-        },
-      });
-    } else {
-      map.addLayer({
-        id,
-        type: 'symbol',
-        source: id,
-        layout: {
-          'icon-image': '{image}',
-          'icon-size': iconScale,
-          'icon-allow-overlap': true,
+  useEffect(() => {
+    if (!map.getSource(id)) {
+      map.addSource(id, {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [],
         },
       });
     }
 
+    map.addLayer({
+      id,
+      type: 'symbol',
+      source: id,
+      filter: ['!has', 'point_count'],
+      layout: {
+        'icon-image': '{image}',
+        'icon-size': iconScale,
+        'icon-allow-overlap': true,
+        'text-field': showTitles ? '{title}' : '',
+        'text-allow-overlap': true,
+        'text-anchor': 'bottom',
+        'text-offset': [0, -2 * iconScale],
+        'text-font': findFonts(map),
+        'text-size': 12,
+      },
+      paint: {
+        'text-halo-color': 'white',
+        'text-halo-width': 1,
+      },
+    });
+
     return () => {
-      if (map.getLayer(id)) {
-        map.removeLayer(id);
-      }
-      if (map.getSource(id)) {
-        map.removeSource(id);
-      }
+      if (map.getLayer(id)) map.removeLayer(id);
+      if (map.getSource(id)) map.removeSource(id);
     };
-  }, [showTitles]);
+  }, [id, iconScale]);
 
   useEffect(() => {
-    map.getSource(id)?.setData({
-      type: 'FeatureCollection',
-      features: markers.map(({ latitude, longitude, image, title }) => ({
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [longitude, latitude],
-        },
-        properties: {
-          image: image || 'default-neutral',
-          title: title || '',
-        },
-      })),
-    });
-  }, [showTitles, markers]);
+    const source = map.getSource(id);
+    if (source) {
+      source.setData({
+        type: 'FeatureCollection',
+        features,
+      });
+    }
+  }, [id, features]);
+
+  useEffect(() => {
+    const updateTextVisibility = () => {
+      const zoom = map.getZoom();
+      const shouldShowTitles = zoom >= 14;
+
+      if (map.getLayer(id)) {
+        map.setLayoutProperty(id, 'text-field', shouldShowTitles ? '{title}' : '');
+      }
+    };
+
+    map.on('zoom', updateTextVisibility);
+    updateTextVisibility(); s
+
+    return () => {
+      map.off('zoom', updateTextVisibility);
+    };
+  }, [id]);
 
   return null;
 };
