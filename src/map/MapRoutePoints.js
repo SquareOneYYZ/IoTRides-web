96b8b6749b07bce2e148085db412b62bf6b40c37
AmanRidesIQ <aman@ridesiq.com>
2025-09-23 08:37:31 +0530

    `change 1`

--- src/map/MapRoutePoints.js
+++ src/map/MapRoutePoints.js
@@ -1,4 +1,4 @@
-import { useId, useEffect, useMemo } from 'react';
+import { useId, useCallback, useEffect, useMemo } from 'react';
 import { map } from './core/MapView';
 import getSpeedColor from '../common/util/colors';
 import { findFonts } from './core/mapUtil';
@@ -18,11 +18,43 @@ const MapRoutePoints = ({ positions, onClick }) => {
     if (!positions.length) return { simplifiedPositions: [] };
 
     const simplified = positions.filter(
-      (p, i) => i === 0 || i === positions.length - 1 || i % 4 === 0,
+      (p, i) => i === 0 || i === positions.length - 1 || i % 4 === 0
     );
 
     return { simplifiedPositions: simplified };
   }, [positions]);
+
+  const onMarkerClick = useCallback(
+    (event) => {
+      event.preventDefault();
+      const feature = event.features[0];
+
+      if (feature) {
+        if (onClick) {
+          onClick(feature.properties.id, feature.properties.index);
+        }
+
+        const maxSpeed = Math.max(...positions.map((pt) => pt.speed));
+        const minSpeed = Math.min(...positions.map((pt) => pt.speed));
+
+        map.getSource(id)?.setData({
+          type: 'FeatureCollection',
+          features: positions.map((p, index) => ({
+            type: 'Feature',
+            geometry: { type: 'Point', coordinates: [p.longitude, p.latitude] },
+            properties: {
+              index,
+              id: p.id,
+              rotation: p.course,
+              color: getSpeedColor(p.speed, minSpeed, maxSpeed),
+              border: p.isReturn ? '#000000' : 'transparent',
+            },
+          })),
+        });
+      }
+    },
+    [onClick, id, positions]
+  );
 
   useEffect(() => {
     map.addSource(id, {
@@ -49,34 +81,37 @@ const MapRoutePoints = ({ positions, onClick }) => {
 
     map.on('mouseenter', id, onMouseEnter);
     map.on('mouseleave', id, onMouseLeave);
+    map.on('click', id, onMarkerClick);
 
     return () => {
       map.off('mouseenter', id, onMouseEnter);
       map.off('mouseleave', id, onMouseLeave);
+      map.off('click', id, onMarkerClick);
 
       if (map.getLayer(id)) map.removeLayer(id);
       if (map.getSource(id)) map.removeSource(id);
     };
-  }, []);
+  }, [onMarkerClick]);
 
   useEffect(() => {
-    if (!positions.length) {
-      return () => {};
-    }
+    if (!positions.length) return;
 
-    const maxSpeed = Math.max(...positions.map((p) => p.speed));
-    const minSpeed = Math.min(...positions.map((p) => p.speed));
+    const maxSpeed = positions.reduce(
+      (a, b) => Math.max(a, b.speed),
+      -Infinity
+    );
+    const minSpeed = positions.reduce((a, b) => Math.min(a, b.speed), Infinity);
 
     const control = new SpeedLegendControl(
       positions,
       speedUnit,
       t,
       maxSpeed,
-      minSpeed,
+      minSpeed
     );
     map.addControl(control, 'bottom-left');
 
-    const setSimplifiedPoints = () => {
+    const showSimplifiedPoints = () => {
       map.getSource(id)?.setData({
         type: 'FeatureCollection',
         features: simplifiedPositions.map((p, index) => ({
@@ -93,53 +128,19 @@ const MapRoutePoints = ({ positions, onClick }) => {
       });
     };
 
-    const setNearbyPoints = (clickedFeature) => {
-      const clickedIndex = clickedFeature.properties.index;
-      const nearby = positions.filter(
-        (_, i) => i >= clickedIndex - 10 && i <= clickedIndex + 10,
-      );
-
-      map.getSource(id)?.setData({
-        type: 'FeatureCollection',
-        features: nearby.map((p, index) => ({
-          type: 'Feature',
-          geometry: { type: 'Point', coordinates: [p.longitude, p.latitude] },
-          properties: {
-            index,
-            id: p.id,
-            rotation: p.course,
-            color: getSpeedColor(p.speed, minSpeed, maxSpeed),
-            border: p.isReturn ? '#000000' : 'transparent',
-          },
-        })),
-      });
-    };
-
-    setSimplifiedPoints();
-
-    map.on('click', id, (event) => {
-      const feature = event.features[0];
-      if (!feature) return;
-      setNearbyPoints(feature);
-
-      if (onClick) {
-        onClick(feature.properties.id, feature.properties.index);
-      }
-    });
+    showSimplifiedPoints();
 
     map.on('click', (e) => {
       const features = map.queryRenderedFeatures(e.point, { layers: [id] });
       if (!features.length) {
-        setSimplifiedPoints();
+        showSimplifiedPoints();
       }
     });
 
     return () => {
       map.removeControl(control);
-      map.off('click', id);
-      map.off('click');
     };
-  }, [positions, simplifiedPositions, speedUnit, t, onClick]);
+  }, [positions, simplifiedPositions, speedUnit, t, id]);
 
   return null;
 };
