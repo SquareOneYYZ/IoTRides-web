cbd4435a8a8ba680f703e21a669ffd839fbbbe9f
AmanRidesIQ <aman@ridesiq.com>
2025-11-15 15:27:02 +0530

    `throttleMiddleware changes`

--- src/store/throttleMiddleware.js
+++ src/store/throttleMiddleware.js
@@ -1,31 +1,58 @@
 import { batch } from 'react-redux';
 
+const threshold = 3;
 const interval = 1500;
+const EXPIRE_TIME = 15000;
 
 export default () => (next) => {
-  let buffer = [];
-  let timer = null;
+  const buffer = [];
+  let throttle = false;
+  let counter = 0;
 
-  function flush() {
-    if (buffer.length > 0) {
-      const latest = buffer[buffer.length - 1];
-      batch(() => next(latest));
-      buffer = [];
+  const deviceLastUpdate = {};
+
+  setInterval(() => {
+    const now = Date.now();
+
+    Object.keys(deviceLastUpdate).forEach((deviceId) => {
+      if (now - deviceLastUpdate[deviceId] > EXPIRE_TIME) {
+        next({
+          type: 'devices/expire',
+          id: deviceId,
+        });
+
+        delete deviceLastUpdate[deviceId];
+      }
+    });
+
+    if (throttle) {
+      if (buffer.length < threshold) {
+        throttle = false;
+      }
+      batch(() => buffer.splice(0, buffer.length).forEach((action) => next(action)));
+    } else {
+      if (counter > threshold) {
+        throttle = true;
+      }
+      counter = 0;
     }
-    timer = null;
-  }
+  }, interval);
 
   return (action) => {
-    if (
-      action.type === 'devices/update'
-      || action.type === 'positions/update'
-    ) {
-      buffer.push(action);
-      if (!timer) {
-        timer = setTimeout(flush, interval);
-      }
-      return null;
+    if (action.type === 'devices/update') {
+      const deviceId = action.payload?.id;
+      if (deviceId) deviceLastUpdate[deviceId] = Date.now();
     }
+
+    if (action.type === 'devices/update' || action.type === 'positions/update') {
+      if (throttle) {
+        buffer.push(action);
+        return null;
+      }
+      counter += 1;
+      return next(action);
+    }
+
     return next(action);
   };
 };
