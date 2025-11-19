a51e67fb9ea0a57f8eebd85a3b390f128187e669
AmanRidesIQ <aman@ridesiq.com>
2025-11-19 22:54:12 +0530

    `changes for throttle`

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,85 +1,92 @@
 import { createSelector } from 'reselect';
 
-export const getPositions = (state) => state.session.positions;
-export const getDevices = (state) => state.devices.items;
-export const getGroups = (state) => state.groups.items;
+// --------------------------------------
+// Base selectors (always safe)
+// --------------------------------------
+export const getPositions = (state) => state.session?.positions || {};
+export const getDevices = (state) => state.devices?.items || {};
+export const getGroups = (state) => state.groups?.items || {};
+
+export const getFilter = (state) => state.filter || { statuses: [], groups: [], keyword: '', sort: '' };
+
 export const getPositionsArray = createSelector(
   [getPositions],
   (positions) => Object.values(positions),
 );
+
+const resolveDeviceGroups = (device, groups) => {
+  const groupIds = [];
+  let gid = device.groupId;
+
+  while (gid) {
+    groupIds.push(gid);
+    gid = groups[gid]?.groupId || 0;
+  }
+
+  return groupIds;
+};
 
 export const getFilteredPositions = createSelector(
   [
     getPositionsArray,
     getDevices,
     getGroups,
-    (state) => state.filter,
-    (_, keyword) => keyword,
-    (_, __, filterMap) => filterMap,
+    getFilter,
+    (_, keyword = '') => keyword,
+    (_, __, filterMap = false) => filterMap,
   ],
   (positions, devices, groups, filter, keyword, filterMap) => {
-    const deviceGroups = (device) => {
-      const groupIds = [];
-      let { groupId } = device;
-      while (groupId) {
-        groupIds.push(groupId);
-        groupId = groups[groupId]?.groupId || 0;
-      }
-      return groupIds;
-    };
+    const { statuses = [], groups: filterGroups = [] } = filter;
+    const lowerKeyword = keyword?.toLowerCase() || '';
 
-    const filtered = Object.values(devices)
-      .filter((device) => !filter.statuses.length || filter.statuses.includes(device.status))
-      .filter((device) => !filter.groups.length || deviceGroups(device).some((id) => filter.groups.includes(id)))
-      .filter((device) => {
-        const lowerCaseKeyword = keyword.toLowerCase();
-        return [device.name, device.uniqueId, device.phone, device.model, device.contact, device.vin].some((s) => s && s.toLowerCase().includes(lowerCaseKeyword));
-      });
+    const filteredDevices = Object.values(devices)
+      .filter((device) => !statuses.length || statuses.includes(device.status))
+      .filter((device) => !filterGroups.length
+        || resolveDeviceGroups(device, groups).some((id) => filterGroups.includes(id)))
+      .filter((device) => [device.name, device.uniqueId, device.model, device.phone, device.vin, device.contact]
+        .some((s) => s?.toLowerCase?.().includes(lowerKeyword)));
 
-    return filterMap
-      ? filtered.map((device) => positions[device.id]).filter(Boolean)
-      : positions;
+    if (!filterMap) return positions;
+
+    return filteredDevices
+      .map((device) => getPositions[device.id])
+      .filter(Boolean);
   },
 );
+
 export const getFilteredDevices = createSelector(
   [
     getDevices,
     getGroups,
-    (state) => state.filter,
-    (_, keyword) => keyword,
-    (_, __, filterSort) => filterSort,
+    getFilter,
+    (_, keyword = '') => keyword,
+    (_, __, sort = '') => sort,
   ],
-  (devices, groups, filter, keyword, filterSort) => {
-    const deviceGroups = (device) => {
-      const groupIds = [];
-      let { groupId } = device;
-      while (groupId) {
-        groupIds.push(groupId);
-        groupId = groups[groupId]?.groupId || 0;
-      }
-      return groupIds;
-    };
+  (devices, groups, filter, keyword, sort) => {
+    const { statuses = [], groups: filterGroups = [] } = filter;
+    const lowerKeyword = keyword?.toLowerCase() || '';
 
-    const filtered = Object.values(devices)
-      .filter((device) => !filter.statuses.length || filter.statuses.includes(device.status))
-      .filter((device) => !filter.groups.length || deviceGroups(device).some((id) => filter.groups.includes(id)))
-      .filter((device) => {
-        const lowerCaseKeyword = keyword.toLowerCase();
-        return [device.name, device.uniqueId, device.phone, device.model, device.contact, device.vin].some((s) => s && s.toLowerCase().includes(lowerCaseKeyword));
-      });
+    let filtered = Object.values(devices)
+      .filter((device) => !statuses.length || statuses.includes(device.status))
+      .filter((device) => !filterGroups.length
+        || resolveDeviceGroups(device, groups).some((gid) => filterGroups.includes(gid)))
+      .filter((device) => [device.name, device.uniqueId, device.model, device.phone, device.vin, device.contact]
+        .some((s) => s?.toLowerCase?.().includes(lowerKeyword)));
 
-    switch (filterSort) {
+    switch (sort) {
       case 'name':
-        filtered.sort((device1, device2) => device1.name.localeCompare(device2.name));
+        filtered = [...filtered].sort((a, b) => a.name.localeCompare(b.name));
         break;
+
       case 'lastUpdate':
-        filtered.sort((device1, device2) => {
-          const time1 = device1.lastUpdate ? new Date(device1.lastUpdate).getTime() : 0;
-          const time2 = device2.lastUpdate ? new Date(device2.lastUpdate).getTime() : 0;
-          return time2 - time1;
+        filtered = [...filtered].sort((a, b) => {
+          const t1 = a.lastUpdate ? new Date(a.lastUpdate).getTime() : 0;
+          const t2 = b.lastUpdate ? new Date(b.lastUpdate).getTime() : 0;
+          return t2 - t1;
         });
         break;
       default:
+        // no sorting
         break;
     }
 
